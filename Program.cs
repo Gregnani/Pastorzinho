45c07a8479391098aa1527cfe6610cece0b08159
gregnani <fgregnani@gmail.com>
2023-04-02 19:48:29 -0300

    atividade concluida

--- Program.cs
+++ Program.cs
@@ -1,6 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Pastorzinho");
-Console.WriteLine("Dó, ré, mi, fá, fá, fá,");
-Console.WriteLine("Dó, ré, dó, ré, ré, ré,");
-Console.WriteLine("Dó, sol, fá, mi, mi, mi,");
-Console.WriteLine("Dó, ré, mi, fá, fá, fá.");
+﻿Console.Write("Dó, "); Console.Beep(1320, 300);
+Console.Write("ré, "); Console.Beep(1485, 300);
+Console.Write("mi, "); Console.Beep(1650, 300);
+Console.Write("fá, "); Console.Beep(1759, 300);
+Thread.Sleep(500);
+Console.Write("fá, "); Console.Beep(1759, 300);
+Console.Write("fá,"); Console.Beep(1759, 300);
+Thread.Sleep(1000);
+
+Console.WriteLine();
+
+Console.Write("Dó, "); Console.Beep(1320, 300);
+Console.Write("ré, "); Console.Beep(1485, 300);
+Console.Write("dó, "); Console.Beep(1320, 300);
+Console.Write("ré, "); Console.Beep(1485, 300);
+Thread.Sleep(500);
+Console.Write("ré, "); Console.Beep(1485, 300);
+Console.Write("ré,"); Console.Beep(1485, 300);
+Thread.Sleep(1000);
+
+Console.WriteLine();
+
+Console.Write("Dó, "); Console.Beep(1320, 300);
+Console.Write("sol, "); Console.Beep(1980, 300);
+Console.Write("fá, "); Console.Beep(1759, 300);
+Console.Write("mi, "); Console.Beep(1650, 300);
+Thread.Sleep(500);
+Console.Write("mi, "); Console.Beep(1650, 300);
+Console.Write("mi,"); Console.Beep(1650, 300);
+Thread.Sleep(1000);
+
+Console.WriteLine();
+
+Console.Write("Dó, "); Console.Beep(1320, 300);
+Console.Write("ré, "); Console.Beep(1485, 300);
+Console.Write("mi, "); Console.Beep(1650, 300);
+Console.Write("fá, "); Console.Beep(1759, 300);
+Thread.Sleep(500);
+Console.Write("fá, "); Console.Beep(1759, 300);
+Console.Write("fá."); Console.Beep(1759, 300);
+Thread.Sleep(1000);
